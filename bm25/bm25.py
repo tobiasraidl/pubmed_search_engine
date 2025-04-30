6ba124b6cef866cf77f8bf0e238731a310da2a46
Tobias Raidl <e11717659@student.tuwien.ac.at>
2025-04-30 11:07:04 +0200

    sync

--- bm25/bm25.py
+++ bm25/bm25.py
@@ -3,18 +3,23 @@ from preprocessing.read_training_data import get_session_key, retrieve_documents
 import numpy as np
 
 session_key = get_session_key()
-query = "nitric oxide synthase"
+query = "Is Hirschsprung disease a mendelian or a multifactorial disorder?"
 documents = retrieve_documents_from_api(query=query, session_key=session_key)
 
 corpus = []
+pmids = []  # <-- Keep track of PMIDs here
+
 for document in documents:
     document_contents = document["title"] + " " + document["abstract"]
     corpus.append(document_contents.split())
+    pmids.append(document["id"])  # <-- Save the PMID
 
 bm25 = BM25Okapi(corpus)
 tokenized_query = query.split()
 
 scores = bm25.get_scores(tokenized_query)
 ranked_doc_indices = np.argsort(scores)[::-1]
+
 for idx in ranked_doc_indices:
-    print(f"Doc {idx}: Score {scores[idx]}")
+    pmid = pmids[idx]  # <-- Use PMID instead of index
+    print(f"PMID {pmid}: Score {scores[idx]}")
