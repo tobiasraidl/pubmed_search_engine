bb174e85285e209e4a35837f47c012f8ac14c493
Tobias Raidl <e11717659@student.tuwien.ac.at>
2025-05-08 15:07:36 +0200

    merged alex' code

--- NLP/alex/src/create_document_embeddings.py
+++ NLP/alex/src/create_document_embeddings.py
@@ -1,10 +1,10 @@
 """
-This scripts embeds the title + abtract into a 200-dimensional 
-"document embedding". The pre-trained BioASQ vector embeddings are 
+This scripts embeds the title + abtract into a 200-dimensional
+"document embedding". The pre-trained BioASQ vector embeddings are
 stored separately: the first word in types.txt corresponds to the first line in vectors.txt.
 
-This model is applied to map each word in a corpus to its corresponding 
-pre-trained vector, if available. Afterward, the document embedding is computed 
+This model is applied to map each word in a corpus to its corresponding
+pre-trained vector, if available. Afterward, the document embedding is computed
 by averaging each dimension across all avaiable word vectors in the document.
 """
 
@@ -22,29 +22,25 @@ PATH_TO_DOCUMENTS = "../../../data/train/documents.json"
 with open(PATH_TO_DOCUMENTS, "r") as file:
     data = json.load(file)
 
-def bioclean(t):
-    return ' '.join(re.sub('[.,?;*!%^&_+():-\[\]{}]', '',t.replace('"', '').replace('/', '').replace('\\', '').replace("'",'').strip().lower()).split()) 
-  
 
 if __name__ == "__main__":
     # load model
     print("Loading embedding model")
-    kv = EmbeddingModel().load_model() # load_model()
+    model = EmbeddingModel()
 
     # embed title and abstract
     print("Embed title and abstract")
     document_embeddings = []
 
-    for i, document in enumerate(tqdm(data[:1], desc="Embedding documents")):
+    for i, document in enumerate(tqdm(data[:20], desc="Embedding documents")):
         document_corpus = document["title"] + document["abstract"]
-        vectors = [kv[token] for token in bioclean(document_corpus).split() if token in kv]
-        document_embedding = np.mean(vectors, axis=0)
+        document_embedding = model.transform_query(document_corpus)
         document_embeddings.append(
-                {
-                    "url" : str(document["url"]),
-                    "document_embedding" : document_embedding.tolist()
-                }
-            )
+            {
+                "url": str(document["url"]),
+                "document_embedding": document_embedding.tolist(),
+            }
+        )
 
     # store document embeddings
     print("Store embeddings to /data")
